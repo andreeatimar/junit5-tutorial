2f366d35951fd8ce9702e14e167151fd4e98ed21
andreea.timar <andreea.timar@fortech.ro>
2023-01-23 13:46:51 +0200

    Initial commit

---
+++ src/test/java/junit5tests/OrderedTestClass1.java
@@ -0,0 +1,38 @@
+package junit5tests;
+
+import org.junit.jupiter.api.*;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class OrderedTestClass1 {
+
+    @BeforeAll
+    void beforeAll() {
+        System.out.println("--This is the before all method.");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("----This is the before each method");
+    }
+
+    @AfterAll
+    void afterAll() {
+        System.out.println("--This is the after all method");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("----This is the after each method");
+    }
+
+    @Test
+    void firstMethod() {
+        System.out.println("This is the first test method.");
+    }
+
+    @Test
+    @DisplayName("US1234 - TC12 - This method is the second one.")
+    void secondMethod() {
+        System.out.println("This is the second test method.");
+    }
+}
